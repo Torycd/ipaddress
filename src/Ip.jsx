9c94cdde4e792f1b6c6e6e1d2d73588bdddb1818
Torycd <vicidundun@gmail.com>
2024-03-26 22:42:54 +0100

    f

--- src/Ip.jsx
+++ src/Ip.jsx
@@ -1,6 +1,6 @@
 import desktopHeader from "./assets/pattern-bg-desktop.png";
 import smallHeader from "./assets/pattern-bg-mobile.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Ip = () => {
   function handleForm(e) {
@@ -17,14 +17,16 @@ const Ip = () => {
         })`,
         backgroundSize: "cover",
         backgroundPosition: "top",
-        
+
         width: "100%",
       }}
     >
-    <h2 className="mt-10 text-center font-bold text-3xl">IP Address Tracker</h2>
+      <h2 className="mt-10 text-center font-bold text-3xl">
+        IP Address Tracker
+      </h2>
       <form onSubmit={handleForm} className="flex justify-center mt-10">
         <input
-          className="rounded-tl-full rounded-bl-full w-[300px] p-5"
+          className="rounded-tl-full rounded-bl-full w-[300px] py-3 px-5"
           name="ip-input"
           minLength="7"
           maxLength="15"
@@ -32,7 +34,9 @@ const Ip = () => {
           placeholder="Search for any Ip Address or Domain"
           pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
         />
-        <button className="bg-black w-[80px] rounded-tr-full rounded-br-full">g</button>
+        <button className="bg-black w-[50px] text-white rounded-tr-full rounded-br-full">
+          <FontAwesomeIcon icon="fa-solid fa-greater-than" className="text-white h-6 w-6"/>
+        </button>
       </form>
     </div>
   );
