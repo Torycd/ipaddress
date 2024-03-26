88b82a3dc11deee8fae7566115de9b974173d672
Torycd <vicidundun@gmail.com>
2024-03-26 22:20:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Ip from "./Ip"
+
+function App() {
+  return (
+    <div>
+      <Ip/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Ip.jsx
@@ -0,0 +1,41 @@
+import desktopHeader from "./assets/pattern-bg-desktop.png";
+import smallHeader from "./assets/pattern-bg-mobile.png";
+
+
+const Ip = () => {
+  function handleForm(e) {
+    e.preventDefault();
+  }
+  return (
+    <div
+      className={`w-dvh h-[500px] md:h-[300px]  text-white ${
+        window.innerWidth >= 375 ? "md:flex flex-col" : ""
+      }`}
+      style={{
+        backgroundImage: `url(${
+          window.innerWidth < 375 ? smallHeader : desktopHeader
+        })`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+        
+        width: "100%",
+      }}
+    >
+    <h2 className="mt-10 text-center font-bold text-3xl">IP Address Tracker</h2>
+      <form onSubmit={handleForm} className="flex justify-center mt-10">
+        <input
+          className="rounded-tl-full rounded-bl-full w-[300px] p-5"
+          name="ip-input"
+          minLength="7"
+          maxLength="15"
+          size="15"
+          placeholder="Search for any Ip Address or Domain"
+          pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
+        />
+        <button className="bg-black w-[80px] rounded-tr-full rounded-br-full">g</button>
+      </form>
+    </div>
+  );
+};
+
+export default Ip;
