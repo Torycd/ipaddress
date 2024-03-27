e3bd192de74c59dcef54d2ae91f48c86e6be9276
Torycd <vicidundun@gmail.com>
2024-03-27 20:23:49 +0100

    lets go

--- src/Ip.jsx
+++ src/Ip.jsx
@@ -1,6 +1,7 @@
 import desktopHeader from "./assets/pattern-bg-desktop.png";
 import smallHeader from "./assets/pattern-bg-mobile.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SlArrowRight } from "react-icons/sl";
+
 
 const Ip = () => {
   function handleForm(e) {
@@ -27,21 +28,11 @@ const Ip = () => {
       <form onSubmit={handleForm} className="flex justify-center mt-10">
         <input
           className="rounded-tl-full rounded-bl-full w-[300px] py-3 px-5"
-          name="ip-input"
-          minLength="7"
-          maxLength="15"
-          size="15"
+          type="number"
           placeholder="Search for any Ip Address or Domain"
-          pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
         />
-        <button className="bg-black w-[50px] text-white rounded-tr-full rounded-br-full">
-          {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-            <path d="M3.4 81.7c-7.9 15.8-1.5 35 14.3 42.9L280.5 256 17.7 387.4C1.9 395.3-4.5 414.5 3.4 430.3s27.1 22.2 42.9 14.3l320-160c10.8-5.4 17.7-16.5 17.7-28.6s-6.8-23.2-17.7-28.6l-320-160c-15.8-7.9-35-1.5-42.9 14.3z" />
-          </svg> */}
-          <FontAwesomeIcon
-            icon="fa-solid fa-greater-than"
-            className=" h-6 w-6"
-          />
+        <button className="bg-black w-[50px] flex justify-center text-white rounded-tr-full rounded-br-full hover:bg-slate-700">
+          <SlArrowRight className="self-center" />
         </button>
       </form>
     </div>
