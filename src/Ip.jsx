e60d47a3b30313407902c2900edcd70c319512ef
Torycd <vicidundun@gmail.com>
2024-03-27 15:36:07 +0100

    yeah

--- src/Ip.jsx
+++ src/Ip.jsx
@@ -35,7 +35,13 @@ const Ip = () => {
           pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
         />
         <button className="bg-black w-[50px] text-white rounded-tr-full rounded-br-full">
-          <FontAwesomeIcon icon="fa-solid fa-greater-than" className="text-white h-6 w-6"/>
+          {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path d="M3.4 81.7c-7.9 15.8-1.5 35 14.3 42.9L280.5 256 17.7 387.4C1.9 395.3-4.5 414.5 3.4 430.3s27.1 22.2 42.9 14.3l320-160c10.8-5.4 17.7-16.5 17.7-28.6s-6.8-23.2-17.7-28.6l-320-160c-15.8-7.9-35-1.5-42.9 14.3z" />
+          </svg> */}
+          <FontAwesomeIcon
+            icon="fa-solid fa-greater-than"
+            className=" h-6 w-6"
+          />
         </button>
       </form>
     </div>
