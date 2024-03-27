2940f8394c53bba653c39e9a0d902f54fba9bc8b
Torycd <vicidundun@gmail.com>
2024-03-27 20:24:04 +0100

    bh

--- src/Ip.jsx
+++ src/Ip.jsx
@@ -1,11 +1,20 @@
 import desktopHeader from "./assets/pattern-bg-desktop.png";
 import smallHeader from "./assets/pattern-bg-mobile.png";
 import { SlArrowRight } from "react-icons/sl";
-
+import { useState } from "react";
 
 const Ip = () => {
+  useState()
+  
   function handleForm(e) {
     e.preventDefault();
+    const fd = new FormData(e.target)
+    const data = Object.fromEntries(fd.entries())
+    
+
+    console.log(data);
+    e.target.reset()
+
   }
   return (
     <div
@@ -27,10 +36,13 @@ const Ip = () => {
       </h2>
       <form onSubmit={handleForm} className="flex justify-center mt-10">
         <input
-          className="rounded-tl-full rounded-bl-full w-[300px] py-3 px-5"
+          className="rounded-tl-full text-black rounded-bl-full w-[300px] py-3 px-5"
           type="number"
-          placeholder="Search for any Ip Address or Domain"
+          name="number"
+          pattern="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$"
+          placeholder="Search for any IP Address or Domain"
         />
+
         <button className="bg-black w-[50px] flex justify-center text-white rounded-tr-full rounded-br-full hover:bg-slate-700">
           <SlArrowRight className="self-center" />
         </button>
