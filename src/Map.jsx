3b6a47112e3f5df151e01cb8351400e6a8fe7096
Torycd <vicidundun@gmail.com>
2024-03-29 13:31:29 +0100

    lets

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,42 +1,53 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
-// import { fetchRequest } from "./hooks/useHttp.js";
 
 const Map = () => {
   const [location, setLocation] = useState([]);
-  useEffect(() => {
-    async function handleLocation() {
-      try {
-        // const places = await fetchRequest();
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = [
-            position.coords.latitude,
-            position.coords.longitude,
-          ];
+  const [loading, setLoading] = useState(true);
 
-          setLocation(sortedPlaces);
+  useEffect(() => {
+    const handleLocation = async () => {
+      try {
+        const position = await new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
         });
+
+        const sortedPlaces = [
+          position.coords.latitude, 
+          position.coords.longitude
+        ];
+
+        setLocation(sortedPlaces);
       } catch (error) {
-        // ..
+        // handle error
+      } finally {
+        setLoading(false);  
       }
-    }
+    };
+
     handleLocation();
-    console.log(location);
-  }, [location]);
+  }, []);
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <MapContainer center={location} zoom={13} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <Marker position={location}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </>
   );
 };
 
