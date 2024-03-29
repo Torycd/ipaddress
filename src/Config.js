6bc8763392a823a47f44fcd4fc84ac44e81ea736
Torycd <vicidundun@gmail.com>
2024-03-30 00:49:48 +0100

    yes

---
+++ src/Config.js
@@ -0,0 +1,4 @@
+// Config.js
+
+const API_KEY = "d8fcb4872ab6453c8b700cbbf68befe4";
+export default API_KEY
--- src/Ip.jsx
+++ src/Ip.jsx
@@ -3,14 +3,14 @@ import smallHeader from "./assets/pattern-bg-mobile.png";
 import { SlArrowRight } from "react-icons/sl";
 import { useEffect, useState, useCallback } from "react";
 import Details from "./Details";
-
+import  API_KEY  from "./Config";
 const Ip = () => {
   const [input, setInput] = useState("");
   const handleForm = useCallback(function handleForm(e) {
     e.preventDefault();
     const fd = new FormData(e.target);
     const data = Object.fromEntries(fd.entries());
-    const number = data['number']
+    const number = data["input"];
     setInput(number);
     // console.log(number);
     e.target.reset();
@@ -18,16 +18,18 @@ const Ip = () => {
 
   useEffect(() => {
     async function fetchRequest() {
-      console.log(input)
-      // to use this input in the API
-      const response = await fetch(
-        "https://geo.ipify.org/api/v2/country?apiKey=at_K17fN7GAmlyTrbnxTGKX480U9xxV0&ipAddress=8.8.8.8"
-      );
+      // console.log(input);
+      const randomIP = input
+      const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=${randomIP}`;
+      const response = await fetch(url);
       const data = response.json();
+      console.log(data);
       return { location: data.location };
     }
-    fetchRequest();
-  }, [handleForm, input]);
+    if (input) {
+      fetchRequest();
+    }
+  }, [input]);
 
   return (
     <div
@@ -47,9 +49,9 @@ const Ip = () => {
       <form onSubmit={handleForm} className="flex justify-center mt-10 mx-5">
         <input
           className="rounded-tl-lg text-black rounded-bl-lg max-w-[500px] w-full md:w-[500px]  py-3 px-5"
-          type="number"
-          name="number"
-          pattern="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$"
+          type="text"
+          name="input"
+          pattern="^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$"
           placeholder="Search for any IP Address or Domain"
         />
 
